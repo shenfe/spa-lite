d7c117d3b9fad3cf81f1821ce1678a70e2a2183b
hengwu <youchuangqiba@163.com>
2018-05-25 01:09:04 +0800

    upd8

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="app">
-    <div>
-      <router-link ></router-link>
-    </div>
-    <router-view/>
-  </div>
+  <el-container>
+    <el-header id="header">Header</el-header>
+    <el-main>
+      <el-container id="main-inner">
+        <el-aside width="200px">
+          <template v-for="item in navigation">
+            <router-link v-bind:key="item.path" :to="item.path">{{ item.name }}</router-link>
+          </template>
+        </el-aside>
+        <el-main id="main">
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-main>
+    <el-footer id="footer">Footer</el-footer>
+  </el-container>
 </template>
 
 <script>
@@ -13,8 +23,8 @@ export default {
   data () {
     return {
       navigation: [
-        '/page1',
-        '/page2'
+        { path: '/page1', name: 'Page1' },
+        { path: '/page2', name: 'Page2' }
       ]
     }
   }
@@ -22,5 +32,20 @@ export default {
 </script>
 
 <style>
-
+html, body {
+  margin: 0;
+  border: 0;
+  padding: 0;
+}
+#header, #footer {
+  padding: 0 calc(50vw - 600px);
+}
+#main {
+  min-height: calc(100vh - 160px);
+}
+#main-inner {
+  margin: auto;
+  width: 1200px;
+  height: 100%;
+}
 </style>
