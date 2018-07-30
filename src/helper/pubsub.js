804e7435c18ab89f231990cffcdd4de6bf11d6d9
Shen <youchuangqiba@163.com>
2018-07-30 11:48:50 +0800

    Update pubsub.js

--- src/helper/pubsub.js
+++ src/helper/pubsub.js
@@ -3,7 +3,8 @@ const type = value => {
 }
 
 const isSimple = value => {
-  switch (type(value)) {
+  const t = type(value)
+  switch (t) {
     case 'number':
     case 'string':
     case 'boolean':
@@ -13,69 +14,73 @@ const isSimple = value => {
   }
 }
 
-const Store = {}
-
-const get = proppath => {
-  let target = Store
-  const parts = proppath.split('.')
-  while (parts.length) {
-    if (isSimple(target)) return
-    let part = parts.shift()
-    target = target[part]
-  }
-  return target
-}
-
-const listeners = {}
-
 const nextTick = (fn, ...args) => {
   setTimeout(() => {
     fn(...args)
   })
 }
 
-const set = (proppath, value, force) => {
-  let target = Store
-  const parts = proppath.split('.')
-  const curPath = []
-  let parent
-  let part
-  while (parts.length) {
-    if (isSimple(target)) {
-      if (!force) {
-        return {
-          code: 1,
-          message: `value error, proppath '${curPath.join('.')}'`
-        }
-      } else {
-        target = parent[part] = {}
-      }
-    }
-    part = parts.shift()
-    curPath.push(part)
-    parent = target
-    target = parent[part]
+export default class Pubsub {
+  constructor() {
+    this._store = {}
+    this._listeners = {}
   }
-  parent[part] = value
 
-  nextTick(() => {
-    const cbs = listeners[proppath]
-    if (!cbs || !cbs.length) return
-    cbs.forEach(cb => {
-      if (type(cb) !== 'function') return
-      cb(value)
+  get(proppath) {
+    let target = this._store
+
+    const parts = proppath.split('.')
+    while (parts.length) {
+      if (isSimple(target)) return
+      let part = parts.shift()
+      target = target[part]
+    }
+    return target
+  }
+
+  set(proppath, value, force) {
+    const listeners = this._listeners
+
+    let target = this._store
+
+    const parts = proppath.split('.')
+    const curPath = []
+    let parent
+    let part
+    while (parts.length) {
+      if (isSimple(target)) {
+        if (!force) {
+          return {
+            code: 1,
+            message: `value error, proppath '${curPath.join('.')}'`
+          }
+        } else {
+          target = parent[part] = {}
+        }
+      }
+      part = parts.shift()
+      curPath.push(part)
+      parent = target
+      target = parent[part]
+    }
+    parent[part] = value
+
+    nextTick(() => {
+      const cbs = listeners[proppath]
+      if (!cbs || !cbs.length) return
+      cbs.forEach(cb => {
+        if (type(cb) !== 'function') return
+        cb(value)
+      })
     })
-  })
-}
+  }
 
-const watch = (proppath, callback, immediate) => {
-  if (!listeners[proppath]) listeners[proppath] = []
-  listeners[proppath].push(callback)
-  immediate && callback(get(proppath))
-}
+  watch(proppath, callback, immediate) {
+    const listeners = this._listeners
 
-export default {
-  set,
-  get,
-  watch
+    if (!listeners[proppath]) listeners[proppath] = []
+    listeners[proppath].push(callback)
+
+    immediate && callback(this.get(proppath))
+  }
 }
